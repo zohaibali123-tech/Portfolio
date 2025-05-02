e9e4512360d061c97a47e539f698fa723779980f
zohaibali123-tech <znondani@gmail.com>
2025-05-02 20:06:39 +0500

    navbar responsivenes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+}
+
 $(document).ready(function () {
 
     /*Skills Data*/
